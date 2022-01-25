16e471f8b5d369a5f93f6519952d77517f254a6f
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-25 23:30:47 +0500

    almost finished

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const socketIO = require("socket.io");
 
 app.use(express.static("public"));
 
-const server = app.listen(9000, console.log(9000));
+const server = app.listen(7000, console.log(7000));
 
 const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("new user");
+});
